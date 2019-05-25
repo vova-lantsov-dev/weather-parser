3985cb1f32465ee2e6e1b4da8883e3a61260dc37
vova-lantsov-dev <vova.lantsov.25.private@gmail.com>
2019-05-25 09:38:15 +0300

    REFACTORING removed unused array variable

--- WeatherParser/Program.cs
+++ WeatherParser/Program.cs
@@ -20,28 +20,26 @@ namespace WeatherParser
             _logger.WriteLog("Application started");
 
             var cancellationSource = new CancellationTokenSource();
-            var tasks = new[]
-            {
-                new WeatherProvider(_logger, new NewOpenWeatherMapParser(_logger, _client, _forecastApiUrl, _apiKey))
-                .GetWeatherForCityAsync("London", cancellationSource.Token)
-                .ContinueWith(task =>
-                {
-                    if (task.IsFaulted)
-                        _logger.WriteExceptionLog(task.Exception);
-                    else if (!task.IsCanceled)
-                        _logger.WriteLog(task.Result);
-                }),
 
-                new WeatherProvider(_logger, new OpenWeatherMapParser(_logger, _client, _apiUrl, _apiKey))
-                .GetWeatherForCityAsync("London", cancellationSource.Token)
-                .ContinueWith(task => 
-                {
-                    if (task.IsFaulted)
-                        _logger.WriteExceptionLog(task.Exception);
-                    else if (!task.IsCanceled)
-                        _logger.WriteLog(task.Result);
-                })
-            };
+            new WeatherProvider(_logger, new NewOpenWeatherMapParser(_logger, _client, _forecastApiUrl, _apiKey))
+            .GetWeatherForCityAsync("London", cancellationSource.Token)
+            .ContinueWith(task =>
+            {
+                if (task.IsFaulted)
+                    _logger.WriteExceptionLog(task.Exception);
+                else if (!task.IsCanceled)
+                    _logger.WriteLog(task.Result);
+            });
+
+            new WeatherProvider(_logger, new OpenWeatherMapParser(_logger, _client, _apiUrl, _apiKey))
+            .GetWeatherForCityAsync("London", cancellationSource.Token)
+            .ContinueWith(task =>
+            {
+                if (task.IsFaulted)
+                    _logger.WriteExceptionLog(task.Exception);
+                else if (!task.IsCanceled)
+                    _logger.WriteLog(task.Result);
+            });
 
             Console.ReadKey();
             cancellationSource.Cancel();
